94c45354ecb5da07c263c336e4e410e731206723
Andrejka <andrejdjokic@ymail.com>
2020-11-27 10:30:48 +0100

    chanching style of main Content

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -11,7 +11,7 @@ import WebDesign from "../../assets/categoriesImages/WebDesign.jpg";
 const MainContent = () => {
   return (
     <div className="mainContent-wrapper">
-      <h3>KONSULTANTSKE USLUGE</h3>
+      <p className="h3 bold mainContentTitle">KONSULTANTSKE USLUGE</p>
       <div className="mainContentDiv">
         <div className="upperContentWrapper">
           <div className="upperContent">
@@ -66,7 +66,7 @@ const MainContent = () => {
           </div>
         </div>
       </div>
-      <h3>BIZNIS PODRŠKA</h3>
+      <p className="h3 bold mainContentTitle">BIZNIS PODRŠKA</p>
       <p>
         U cilju efikasnije poslovne logistike preduzeća, klijentima su na
         raspolaganju usluge poslovnih partnera iz oblasti:
@@ -74,27 +74,27 @@ const MainContent = () => {
       <div className="lowerContent">
         <div className="lowerCard">
           <img src={Accountant} alt="Accountant" className="categoryImage" />
-          <p>Računovodstvene usluge</p>
+          <p className="h4 subtitles">Računovodstvene usluge</p>
         </div>
         <div className="lowerCard">
           <img src={WebDesign} alt="Accountant" className="categoryImage" />
-          <p>Izrada web sajtova i aplikacija</p>
+          <p className="h4 subtitles">Izrada web sajtova i aplikacija</p>
         </div>
         <div className="lowerCard">
           <img src={Cosmetics} alt="Cosmetics" className="categoryImage" />
-          <p>Nabavka sredstava za hemiju</p>
+          <p className="h4 subtitles">Nabavka sredstava za hemiju</p>
         </div>
         <div className="lowerCard">
           <img src={Marketing} alt="Marketing" className="categoryImage" />
-          <p>Online marketing</p>
+          <p className="h4 subtitles">Online marketing</p>
         </div>
         <div className="lowerCard">
           <img src={Dictionary} alt="Dictionary" className="categoryImage" />
-          <p>Usluge prevođenja – engleski jezik</p>
+          <p className="h4 subtitles">Usluge prevođenja – engleski jezik</p>
         </div>
         <div className="lowerCard">
           <img src={Taxi} alt="Taxi" className="categoryImage" />
-          <p>Taksi prevoz</p>
+          <p className="h4 subtitles">Taksi prevoz</p>
         </div>
       </div>
     </div>
