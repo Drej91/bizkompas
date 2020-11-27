7db067417d732cefcc0e472c38513977a1c7f7d6
Andrejka <andrejdjokic@ymail.com>
2020-11-27 09:34:30 +0100

    navbar css fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import { BiAlignRight } from "react-icons/bi";
-import Logo from "../../assets/Images/Logo.svg";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
-  const [showLinks, setShowLinks] = useState("");
 
   return (
     <>
@@ -14,50 +11,43 @@ const Navbar = () => {
         <div className="navbar-container">
           <div className="logo-wrapper">
             <Link to="/" className="navbar-logo" onClick={() => setActive("")}>
-              {/* <img src={Logo} alt="logo" className="logo" /> */}
-
               <span>
-                <p>Bizkompas</p>
+                <p className="logoText caps font-family-inherit font-size-inherit">
+                  Bizkompas
+                </p>
               </span>
             </Link>
           </div>
-          {showLinks === true ? (
-            <div className="links-wrapper">
-              <Link
-                to="/services"
-                className={
-                  active === "services" ? "active-link" : "navbar-link"
-                }
-                onClick={() => {
-                  setActive("services");
-                }}
-              >
-                <p>Usluge</p>
-              </Link>
-              <Link
-                to="/aboutUs"
-                className={active === "aboutUs" ? "active-link" : "navbar-link"}
-                onClick={() => {
-                  setActive("aboutUs");
-                }}
-              >
-                <p>O nama</p>
-              </Link>
-              <Link
-                to="/contact"
-                className={active === "contact" ? "active-link" : "navbar-link"}
-                onClick={() => {
-                  setActive("contact");
-                }}
-              >
-                <p>Kontakt</p>
-              </Link>
-            </div>
-          ) : null}
-          <BiAlignRight
-            className="navbarIcon"
-            onClick={() => setShowLinks(!showLinks)}
-          />
+
+          <div className="links-wrapper">
+            <Link
+              to="/services"
+              className={active === "services" ? "active-link" : "navbar-link"}
+              onClick={() => {
+                setActive("services");
+              }}
+            >
+              <p>Usluge</p>
+            </Link>
+            <Link
+              to="/aboutUs"
+              className={active === "aboutUs" ? "active-link" : "navbar-link"}
+              onClick={() => {
+                setActive("aboutUs");
+              }}
+            >
+              <p>O nama</p>
+            </Link>
+            <Link
+              to="/contact"
+              className={active === "contact" ? "active-link" : "navbar-link"}
+              onClick={() => {
+                setActive("contact");
+              }}
+            >
+              <p>Kontakt</p>
+            </Link>
+          </div>
         </div>
       </nav>
     </>
