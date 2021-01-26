49226f85b3a7a7ed1a0888f5a24b9a380f5883b2
Andrejka <andrejdjokic@ymail.com>
2021-01-26 10:56:29 +0100

    add usefulLinks

--- src/Components/UsefulLinks/UsefulLinks.jsx
+++ src/Components/UsefulLinks/UsefulLinks.jsx
@@ -4,8 +4,37 @@ import "../UsefulLinks/UsefulLinks.css";
 const UsefulLinks = () => {
   return (
     <div className="container">
-      <p className="h2 bold">Korisni linkovi</p>
-      <p className="h3 regular">Uskoro na našem sajtu. Hvala na poverenju.</p>
+      <div className="titleContainer">
+        <p className="h2 bold">Korisni linkovi</p>
+        <p>
+          Ovde možete pronaći neke od korisnih linkova koji će vam pomoći
+          prilikom otvaranja ili vođenja vašeg biznisa.
+        </p>
+      </div>
+      <div className="linksWrapper">
+        <div className="singleLinkWrapper">
+          <p className="h4 bold">Poreska uprava:</p>
+          <p className="regular">
+            <a href="https://www.purs.gov.rs/">www.purs.gov.rs</a>
+          </p>
+        </div>
+        <div className="singleLinkWrapper">
+          <p className="h4 bold">Poreska uprava - PIB registar:</p>
+          <p className="regular">
+            <a href="https://www.purs.gov.rs/pib.html">
+              www.purs.gov.rs/pib.html
+            </a>
+          </p>
+        </div>
+        <div className="singleLinkWrapperLast">
+          <p className="h4 bold">Poreska uprava - Kontakt centar:</p>
+          <p className="regular">
+            <a href="https://www.purs.gov.rs/kontakt/kontakt-centar.html">
+              www.purs.gov.rs/kontakt/kontakt-centar.html
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
